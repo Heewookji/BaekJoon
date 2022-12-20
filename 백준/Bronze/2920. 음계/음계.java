b8bccef99cdbb3a9e7b6b251ac417eb6090c3cc3
Heewookji <46276533+Heewookji@users.noreply.github.com>
2022-12-20 22:02:19 +0900

    [Bronze II] Title: 음계, Time: 112 ms, Memory: 12844 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -3,13 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int length = 8;
-        boolean isAsc = true;
-        boolean isDesc = true;
-        for(int i = 1; i <= length; i++) {
+        int length = 9;
+        boolean isAsc = true, isDesc = true;
+        for(int i = 1; i < length; i++) {
             int num = sc.nextInt();
             if(num != i) isAsc = false;
-            if(num != length - i + 1) isDesc = false;
+            if(num != length - i) isDesc = false;
         }
         System.out.println(isAsc ? "ascending" : isDesc ? "descending": "mixed" );
     }
